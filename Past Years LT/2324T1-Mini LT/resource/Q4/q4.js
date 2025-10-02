d1b6f2025de4b134b72c004155bbb55529b6756c
Jie Ching Lim <limjieching@gmail.com>
2025-10-02 15:59:21 +0800

    Completed 2324MLT

--- Past Years LT/2324T1-Mini LT/resource/Q4/q4.js
+++ Past Years LT/2324T1-Mini LT/resource/Q4/q4.js
@@ -2,22 +2,81 @@
     NAME:
     EMAIL: 
 */
-var url1 = "sales1.json"
-var url2 = "sales2.json"
-
+var url1 = "sales1.json";
+var url2 = "sales2.json";
 
 function mergeSales(elem, title, url1, url2) {
-    var data = []
-    // ADD CODE HERE
-    
-    display(elem, data, title)
-     
+  var data = [];
+  // ADD CODE HERE
+  axios.get(url1).then((response) => {
+    // retrieve sales from sales
+    let sales1 = response.data;
+    console.log(sales1);
+
+    axios.get(url2).then((response) => {
+      // retrieve sales from sales
+      let sales2 = response.data;
+      console.log(sales2);
+
+      sales = sales1.books.concat(sales2.books);
+
+      let authorSales = {};
+
+      for (let item of sales) {
+        if (item.author in authorSales) {
+          authorSales[item.author] += item.sales;
+        } else {
+          authorSales[item.author] = item.sales;
+        }
+      }
+
+
+      for (let author in authorSales) {
+        let x = { author: author, sales: authorSales[author] };
+        data.push(x);
+      }
+      console.log(data)
+      display(elem, data, title);
+    });
+  });
 }
 
 function sortSales(elem, title, url1, url2) {
-    var data = []
-    // ADD CODE HERE
-    
-    display(elem, data, title)
-     
+  var data = [];
+  // ADD CODE HERE
+  axios.get(url1).then((response) => {
+    // retrieve sales from sales
+    let sales1 = response.data;
+    console.log(sales1);
+
+    axios.get(url2).then((response) => {
+      // retrieve sales from sales
+      let sales2 = response.data;
+      console.log(sales2);
+
+      sales = sales1.books.concat(sales2.books);
+
+      let authorSales = {};
+
+      for (let item of sales) {
+        if (item.author in authorSales) {
+          authorSales[item.author] += item.sales;
+        } else {
+          authorSales[item.author] = item.sales;
+        }
+      }
+
+      for (let author in authorSales) {
+        if (authorSales[author] >= 5) {
+        let x = { author: author, sales: authorSales[author] };
+        data.push(x);}
+      }
+
+      data.sort(function(a, b){
+                return b.sales - a.sales
+            })
+      console.log(data)
+      display(elem, data, title);
+    });
+  });
 }
