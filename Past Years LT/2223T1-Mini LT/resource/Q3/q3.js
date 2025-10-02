318402d3bf00eb721bf2b62aaddc5a7f87b82d3a
Jie Ching Lim <limjieching@gmail.com>
2025-10-02 18:12:48 +0800

    completed 2223MLT

--- Past Years LT/2223T1-Mini LT/resource/Q3/q3.js
+++ Past Years LT/2223T1-Mini LT/resource/Q3/q3.js
@@ -2,28 +2,49 @@
  Name: 
  Email: 
  */
- 
- // DO NOT EDIT: START
+
+// DO NOT EDIT: START
 const jokeTable = document.getElementById("joke-table");
 const jokeTableTbody = jokeTable.getElementsByTagName("tbody")[0];
 const jokeText = document.getElementById("jokeText");
 // DO NOT EDIT: END
+
+const API_URL = "q3_getJoke.php";
 
 function getSampleJokes() {
   // DO NOT EDIT: START
   while (jokeTableTbody.lastChild) {
     jokeTableTbody.lastChild.remove();
   }
-  
+
   let serial = 1;
   // DO NOT EDIT: END
-  
+
   // To be completed...
-  
+  for(joke of sampleJokes) {
+    if (joke.type == "twopart"){
+      let tr = document.createElement("tr");
+      jokeTableTbody.appendChild(tr);
+
+      let content = [serial++, joke.category, joke.setup, joke.delivery];
+
+      for (item of content) {
+        let td = document.createElement("td");
+        td.innerHTML = item;
+        tr.appendChild(td);
+      }
+    }
+  }
 }
 
-function getJoke() {
+async function getJoke() {
   // To be completed...
-
+  let query = document.getElementById('categoriesDD').value;
+  try {
+    const response = await axios.get(API_URL, {params: {query: query}});
+    console.log(response);
+    document.getElementById("jokeText").innerText = response.data
+  } catch (error) {
+    console.error("Error fetching stations:", error);
+  }
 }
-
