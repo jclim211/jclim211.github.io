e16b5cafc1593549d11b2de8dfce215255fd0d8f
Jie Ching Lim <limjieching@gmail.com>
2025-10-03 09:18:47 +0800

    completed 2122LT

--- Past Years LT/2122T1-Mini LT/resources/Q2/slideshow.js
+++ Past Years LT/2122T1-Mini LT/resources/Q2/slideshow.js
@@ -3,6 +3,7 @@
     Email:  YOUR SMU EMAIL ID GOES HERE (lee.oppa.2020)
 */
 
+const API_URL = "api/info.php";
 
 /* DO NOT CHANGE THIS FUNCTION's NAME */
 function display_default() {
@@ -13,6 +14,18 @@ function display_default() {
     // Part A
     // YOUR CODE GOES HERE
     // YOU MAY ADD MORE FUNCTION(S) IF YOU WISH
+    axios
+    .get(API_URL)
+    .then(response => {
+        console.log(response.data);
+
+        for (data of response.data.military_statistics){
+            console.log(data)
+        }
+    })
+    .catch(error => {
+        console.log(error.message);
+    })
 
     
     console.log("===[END] display_default() ===")
