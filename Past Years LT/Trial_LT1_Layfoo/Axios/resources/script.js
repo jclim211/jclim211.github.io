c120aeeb1dd6eee722906ab31e0627daa7d7b5b4
Jie Ching Lim <limjieching@gmail.com>
2025-10-03 15:49:28 +0800

    Partial Complete Trial LT1 Layfoo

--- Past Years LT/Trial_LT1_Layfoo/Axios/resources/script.js
+++ Past Years LT/Trial_LT1_Layfoo/Axios/resources/script.js
@@ -1,16 +1,48 @@
 // Your answer goes here
 
 // Feel free to add more code
-
+const api_endpoint_url = "api/products.php"
 function populateCategories() {
    // TODO
    console.log('populateCategories()');
+   axios
+   .get(api_endpoint_url)
+   .then((response) => {
+      console.log(response.data);
+      for (item in response.data){
+         console.log(item);
+         let option = document.createElement("option");
+         option.value = item;
+         option.innerText = item;
+         document.getElementById("category").appendChild(option);
+      }
+   }
+   )
 }
 
 
-function updateProductTable() {
+async function updateProductTable() {
    // TODO
    console.log('updateProductTable()');
+
+   const categoryEle = document.getElementById('category');
+   const outputEle = document.getElementById('output');
+
+   if (categoryEle.value === "") {
+      outputEle.innerText = "Select a product category."
+      return;
+   }
+
+   try {
+   const response = await axios.get(url, {
+      params: {
+         category: categoryEle.value
+      }
+   });
+
+   while (outputEle.lastChild) outputEle.lastChild.remove();
+
+   
 }
 
 
